5e5473a638c49086a8b7d1add5466c42a731688e
findus19 <ivanexs@mail.ru>
2020-02-18 20:54:42 +0300

    dz3

--- script/script.js
+++ script/script.js
@@ -1,16 +1,37 @@
 'use strict'
 let tank = document.querySelector('.tank'),
     human = document.querySelector('.human'),
+    reset = document.querySelector('.reset'),
     count = 0;
-let humanRan = function(){
+let flyInterval;    
+let flyAnimate = function(){
+  flyInterval = requestAnimationFrame(flyAnimate);
   count++;
   human.style.top = count + 'px';
-  human.style.left = count + 'px';
-  tank.style.left = count + 'px';
-  console.log(count);
-  if(count < 700){
-    setTimeout(humanRan, 10);
+  human.style.left = count + 100 + 'px';
+  tank.style.left = count*2 + 'px';
+  if(count > 410){
+    cancelAnimationFrame(flyInterval);
   }
 }
 
-humanRan();
+let animate = false;
+reset.addEventListener('click', function(){
+
+  tank.style.top =  '130px';
+  tank.style.left = '100px';
+  human.style.top =  '0px';
+  human.style.left = '0px';
+  cancelAnimationFrame(flyInterval);
+  flyAnimate();
+});
+
+document.addEventListener('click',function(){
+  if(animate){
+    requestAnimationFrame(flyAnimate);
+    animate = false;
+  }else {
+    animate = true;
+    cancelAnimationFrame(flyInterval);
+  }
+});
